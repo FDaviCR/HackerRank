61f4d78ac21d1e0d91be41357350dbd3c872bb8f
Davi <davicamiloribeiro@gmail.com>
2022-04-08 11:28:14 -0300

    Removendo comentarios

--- Revising the Select Query I/query.sql
+++ Revising the Select Query I/query.sql
@@ -1,9 +1,2 @@
-/*
-PROBLEM:
-
-Query all columns for all American cities in the CITY table with populations larger than 100000. The CountryCode for America is USA.
-The CITY table is described as follows:
-*/
-
 SELECT * FROM CITY c(NOLOCK) 
 WHERE COUNTRYCODE = "USA" AND POPULATION > 100000;
--- Revising the Select Query II/query.sql
+++ Revising the Select Query II/query.sql
@@ -1,9 +1,2 @@
-/*
-PROBLEM:
-
-Query the NAME field for all American cities in the CITY table with populations larger than 120000. The CountryCode for America is USA.
-The CITY table is described as follows:
-*/
-
 SELECT NAME FROM CITY c(nolock) 
 WHERE COUNTRYCODE = "USA" AND POPULATION > 120000;
